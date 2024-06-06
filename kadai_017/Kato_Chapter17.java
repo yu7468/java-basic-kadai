ee776ccc4b241ae655f5dec976e97b9ddc9c8b91
yu7468 <cherish7468@gmail.com>
2024-06-06 17:48:01 +0900

    Kato_Chapter17.javaを追加

---
+++ kadai_017/Kato_Chapter17.java
@@ -0,0 +1,15 @@
+package JavaKadai;
+
+public abstract class Kato_Chapter17 {
+	public String familyName = "加藤";
+	public String givenName;
+	public String address = "東京都中野区〇×";
+	
+	
+	public void commonIntroduce() {
+	};
+	abstract public void eachIntroduce();
+	public void execIntroduce() {
+	};
+
+}
