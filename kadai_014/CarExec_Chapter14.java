6b5b1cfd2fe063ccdcda444fdd4dd026b1197fa7
yu7468 <cherish7468@gmail.com>
2024-06-06 16:20:35 +0900

    CarExec_Chapter14.javaを追加

---
+++ kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,11 @@
+package JavaKadai;
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Car_Chapter14 car = new Car_Chapter14();
+		car.gearChange(3);
+		car.run();
+	}
+}
