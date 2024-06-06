ceed52ab613bbae891c3a072e5ef03964f748196
yu7468 <cherish7468@gmail.com>
2024-06-06 17:48:55 +0900

    KatoHanako_Chapter17.javaを追加

---
+++ kadai_017/KatoHanako_Chapter17.java
@@ -0,0 +1,17 @@
+package JavaKadai;
+
+public class KatoHanako_Chapter17 extends Kato_Chapter17 {
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	public void execIntroduce() {
+		System.out.println("名前は" + familyName + this.givenName + "です");
+		System.out.println("住所は" + address + "です");
+		eachIntroduce();
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です"+ "\n");
+	}
+
+}
