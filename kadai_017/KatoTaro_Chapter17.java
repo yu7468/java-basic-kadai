e76bc749777868ea759ccd0b757567a290969925
yu7468 <cherish7468@gmail.com>
2024-06-08 17:47:41 +0900

    commonIntroduce()内容を親クラスに移動

--- kadai_017/KatoTaro_Chapter17.java
+++ kadai_017/KatoTaro_Chapter17.java
@@ -8,7 +8,7 @@ public class KatoTaro_Chapter17 extends Kato_Chapter17 {
 	
 	public void execIntroduce() {
 		System.out.println("名前は" + familyName + this.givenName + "です");
-		System.out.println("住所は" + address + "です");
+		commonIntroduce();
 		eachIntroduce();
 	}
 	
