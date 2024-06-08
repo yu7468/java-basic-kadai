0b58f57e5c16331bef45c283924924a3d119faaa
yu7468 <cherish7468@gmail.com>
2024-06-08 17:47:57 +0900

    commonIntroduce()内容を親クラスに移動

--- kadai_017/KatoHanako_Chapter17.java
+++ kadai_017/KatoHanako_Chapter17.java
@@ -6,7 +6,7 @@ public class KatoHanako_Chapter17 extends Kato_Chapter17 {
 	}
 	public void execIntroduce() {
 		System.out.println("名前は" + familyName + this.givenName + "です");
-		System.out.println("住所は" + address + "です");
+		commonIntroduce();
 		eachIntroduce();
 	}
 	
