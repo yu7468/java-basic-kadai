b4cdbb687590720de6ce93613687153b208bfec9
yu7468 <cherish7468@gmail.com>
2024-06-11 15:53:49 +0900

    JyankenExec.Chapter24.javaを追加

---
+++ kadai_024/JyankenExec_Chapter24.java
@@ -0,0 +1,11 @@
+package JavaKadai;
+
+public class JyankenExec_Chapter24 {
+
+		public static void main(String[] args) {
+			Jyanken_Chapter24 jyanken = new Jyanken_Chapter24();
+			jyanken.getMyChoice();
+			jyanken.getRandom();
+			jyanken.playGame();
+		}
+}
